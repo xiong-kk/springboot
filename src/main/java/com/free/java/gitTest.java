519804de485009ea209821214f3c0c68a4349ebc
free <hunter.com>
2023-10-15 16:35:21 +0800

    first commit

---
+++ src/main/java/com/free/java/gitTest.java
@@ -0,0 +1,11 @@
+package com.free.java;
+
+/**
+ * @author free
+ * @create 2023-10-15-16:32
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
